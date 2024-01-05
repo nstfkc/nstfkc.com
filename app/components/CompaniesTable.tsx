af16d47425257a4baffddaad4025264f96c47244
Enes Tufekci <enesxtufekci@gmail.com>
2024-01-05 11:17:40 +0100

    fix table actions

--- app/components/CompaniesTable.tsx
+++ app/components/CompaniesTable.tsx
@@ -210,7 +210,7 @@ const TableFilters = () => {
           <span>Sort</span>
         </Button>
       </div>
-      <div className="flex items-center gap-2 hidden md:block">
+      <div className="flex items-center gap-2 hidden md:flex">
         <Button>
           <TbSettings className="opacity-60" />
           <span>View Settings</span>
