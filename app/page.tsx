6973b4492df7c2cd8edd4892536065114eeb8a9e
Enes Tufekci <enesxtufekci@gmail.com>
2024-01-18 11:55:02 +0100

    compiled work

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,49 @@
 import Link from "next/link";
+import { work } from "./data";
+import Image from "next/image";
+import { LuArrowUpRightSquare, LuCode2, LuLink2 } from "react-icons/lu";
 
 const Home = () => {
   return (
-    <div>
-      <Link href="/attio">Attio CRM</Link>
+    <div className="bg-gradient-to-tr from-zinc-200 to-zinc-50 text-zinc-700">
+      <div className="container max-w-xl mx-auto flex flex-col gap-8 py-8">
+        {work.map((item) => (
+          <div
+            key={item.url}
+            className="flex flex-col gap-2 p-4 bg-white/50 rounded-[14px]"
+          >
+            <div className="flex justify-between items-center">
+              <div className="text-lg font-semibold">{item.label}</div>
+              <div className="flex gap-4 items-center">
+                <a
+                  className="flex items-center gap-1 opacity-75 text-sm px-2 py-1 rounded-md bg-black/5"
+                  href={`https://github.com/nstfkc/ui-ux/tree/main/app${item.url}`}
+                >
+                  Code <LuCode2 />
+                </a>
+                <Link
+                  href={item.url}
+                  className="flex items-center gap-1 opacity-75 text-sm px-2 py-1 rounded-md bg-black/5"
+                >
+                  Preview <LuArrowUpRightSquare />
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              {item.images.map((image) => {
+                return (
+                  <div
+                    key={image.src}
+                    className="rounded-[10px] overflow-hidden border"
+                  >
+                    <Image alt={item.label} src={image} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
