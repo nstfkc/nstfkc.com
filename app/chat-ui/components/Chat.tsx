1fe3f767fd593a0bdffd268554d927bd7a6eed8a
Enes Tufekci <enesxtufekci@gmail.com>
2024-02-11 11:17:34 +0100

    arctan fn

--- app/chat-ui/components/Chat.tsx
+++ app/chat-ui/components/Chat.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { useScroll, motion, useVelocity } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { useScroll, motion, useVelocity, useTransform } from "framer-motion";
 
-const Channel = () => {
+const Channel = (props: { id: string }) => {
   return (
     <div className="py-2">
       <div className="border p-4 rounded-xl bg-white/80 shadow-md backdrop-blur-[4px]">
-        <div>#General</div>
+        <div>#General {props.id}</div>
         <div className="flex gap-2 h-0 overflow-hidden">
           <div className="size-12 min-w-12 flex rounded-md bg-stone-800"></div>
           <div className="flex flex-col">
@@ -26,7 +26,7 @@ const Channel = () => {
 };
 
 const ITEM_HEIGHT = 74;
-const ITEM_COUNT = 10;
+const ITEM_COUNT = 20;
 
 interface CalculateTopParams {
   totalItems: number;
@@ -39,41 +39,119 @@ function top(x: number, a = 1) {
   return Number((1 / Math.pow(Math.E, Math.pow(x * a, 2))).toFixed(2));
 }
 
+function top2(x: number, a = 1) {
+  return Math.atan(x / a);
+}
+
+function calcX(x: number, a = 1) {
+  return 1 / Math.pow(Math.E, Math.pow(x, 2) * a);
+}
+
+function calcX2(x: number, a = 1) {
+  return Math.sqrt((Math.asin(x) / 2) * Math.E);
+}
+
 export const Chat = () => {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({ container: ref });
   const scrollVelocity = useVelocity(scrollYProgress);
 
-  useEffect(() => {
-    return scrollVelocity.on("change", (latest) => {});
-  }, [scrollVelocity]);
-
-  const index = 5;
-
-  console.table(
-    Array.from({ length: ITEM_COUNT }).map((_, i) => [
-      index - i,
-      top(index - i, 0.2) * 100,
-    ])
+  const [index, setIndex] = useState(0);
+  const [a, setA] = useState(1.42);
+  const [b, setB] = useState(1.4);
+  const [multiplier, setMultiplier] = useState(160);
+  const [offset, setOffset] = useState(120);
+  const scrollY = useTransform(
+    () => scrollYProgress.get() * ITEM_COUNT * ITEM_HEIGHT
   );
 
+  useEffect(() => {
+    return scrollYProgress.on("change", (latest) => {
+      const index = Math.floor(latest * ITEM_COUNT);
+      setIndex(index);
+    });
+  }, [scrollYProgress]);
+
+  const t = Array.from({ length: ITEM_COUNT }).map((_, i) => [
+    top2(top2(i - index, a), b) * multiplier + offset,
+  ]);
+
   return (
-    <div className="container max-w-2xl mx-auto flex items-center h-full">
-      <div ref={ref} className="relative w-full h-[400px] overflow-scroll">
-        <div style={{ height: `${ITEM_HEIGHT * ITEM_COUNT}px` }}></div>
-        {Array.from({ length: ITEM_COUNT }).map((_, i) => (
+    <div className="container max-w-2xl mx-auto h-full">
+      <div>
+        <div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="">a: {a}</label>
+            <input
+              type="range"
+              min={0}
+              max={10}
+              step={0.01}
+              value={a}
+              onChange={(e) => setA(Number(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="">b: {b}</label>
+            <input
+              type="range"
+              min={0}
+              max={10}
+              step={0.1}
+              value={b}
+              onChange={(e) => setB(Number(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="">multiplier: {multiplier}</label>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              step={10}
+              value={multiplier}
+              onChange={(e) => setMultiplier(Number(e.target.value))}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="">offset: {offset}</label>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              step={10}
+              value={offset}
+              onChange={(e) => setOffset(Number(e.target.value))}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center h-full">
+        <div
+          ref={ref}
+          className="relative w-full h-[400px] overflow-scroll bg-red-100"
+        >
           <motion.div
-            key={i}
-            style={{
-              top: `${(top(index - i, 0.1) * ITEM_HEIGHT) / 2}px`,
-              zIndex: top(index - i, 0.1) * 100,
-            }}
-            data-scale={top(i + 0, 1)}
-            className="absolute"
+            className="relative bg-green-100"
+            style={{ top: scrollY, height: "400px" }}
           >
-            <Channel />
+            {Array.from({ length: ITEM_COUNT }).map((_, i) => (
+              <motion.div
+                key={i}
+                style={{
+                  top: `${t[i]}px`,
+                  zIndex: top(index - i, 0.1) * 100,
+                  scale: top(index - i, 0.1),
+                }}
+                data-scale={top(i + 0, 1)}
+                className="absolute"
+              >
+                <Channel id={`${i}`} />
+              </motion.div>
+            ))}
           </motion.div>
-        ))}
+          <div style={{ height: `${ITEM_HEIGHT * ITEM_COUNT}px` }}></div>
+        </div>
       </div>
     </div>
   );
